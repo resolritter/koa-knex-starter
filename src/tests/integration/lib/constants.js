f41d0ec6f7f2c51dc79abc163f40db85e5437475
resolritter <resolritter@users.noreply.github.com>
2020-11-27 06:31:47 -0300

    have 'port_acquisition_server' as a package dependency

--- src/tests/integration/lib/constants.js
+++ src/tests/integration/lib/constants.js
@@ -1,9 +1,8 @@
 const path = require("path")
 
-// FIXME have submodules or package dependency
 module.exports.portAcquisitionServerLauncher = path.join(
   __dirname,
-  "../../../../../port_acquisition_server/main.js",
+  "../../../../node_modules/port_acquisition_server/main.js",
 )
 module.exports.portAcquisitionAddressFile = path.join(
   __dirname,
